2279244321275e36b7154473e87d28b35e822d4b
Sergey Zelenov <S.Zelenov23@mail.ru>
2023-04-19 22:49:22 +0300

    Задача 10

---
+++ Задача 10.cs
@@ -0,0 +1,5 @@
+﻿    int random = new Random().Next(100, 1000);
+    int SecondNamber = random - (random/100)*100;
+    Console.WriteLine("Случайное число: " + random);
+    Console.WriteLine("Вторая цифра: " + SecondNamber/10);
+   
